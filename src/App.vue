1a31675add517e270cd6c21ff6d204fb5c954c65
haisen <lhsaq2009@163.com>
2024-01-26 01:05:53 +0800

    None

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,17 @@
 </script>
 
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
 
-        <div class="wrapper">
-            <HelloWorld msg="You did it! 5555"/>
+    <header style="font-size: 1.4em;">
 
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-        </div>
+        <ul>
+            <li>第一步：克隆项目 <a href="https://github.com/lhsaq2009/Github-Pages-Demo">Github-Pages-Demo</a></li>
+            <li>第二步：你的仓库 -> Settings -> Pages -> Build and deployment：选择 GitHub Actions</li>
+        </ul>
+
+        <img class="logo" src="@/assets/settings.png" style="margin-top: 1em;"/>
+
     </header>
 
-    <RouterView/>
+    <!-- <RouterView/> -->
 </template>
